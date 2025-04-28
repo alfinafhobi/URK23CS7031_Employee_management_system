7c5d01460c023bfb0c8e92b9a1ef06ef2df196e0
alfinafhobi <153580613+alfinafhobi@users.noreply.github.com>
2025-04-28 22:40:38 +0530

    Add files via upload

---
+++ employeedatabase.sql
@@ -0,0 +1,7 @@
+create database employeemanagement;
+use employeemanagement;
+create table login(username varchar(20),password varchar(20));
+select * from login;
+insert into login values("alfina","123456");
+create table employee(name varchar(40),fname varchar(20),dob varchar(40),salary varchar(10),address varchar(40),phone varchar(20),email varchar(40),education varchar(20),designation varchar(20),aadhar varchar(20),empid varchar(20));
+select * from employee;
